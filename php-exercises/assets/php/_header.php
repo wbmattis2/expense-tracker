03c9b27e8e52bf1ac7b84ce6ac1cc63107093919
Benny" Wayne Mattis <37753727+wbmattis2@users.noreply.github.com>
2023-04-09 07:46:06 -0400

    added exercises and specifications.txt

---
+++ php-exercises/assets/php/_header.php
@@ -0,0 +1,29 @@
+<header>
+    <h1>PHP Exercises</h1>
+    <p>Coded by Benny Mattis.</p>
+    <nav>
+        <ul>
+            <li>
+                <a href="./index.php" title="Home">Home</a>
+            </li>
+            <li>
+                <a href="./exercise_1.php?title=1">Exercise 1</a>
+            </li>
+            <li>
+                <a href="./exercise_2.php?title=2">Exercise 2</a>
+            </li>
+            <li>
+                <a href="./exercise_3.php?title=3">Exercise 3</a>
+            </li>
+            <li>
+                <a href="./exercise_4.php?title=4">Exercise 4</a>
+            </li>
+            <li>
+                <a href="./exercise_5.php?title=5">Exercise 5</a>
+            </li>
+            <li>
+                <a href="./exercise_6.php?title=6">Exercise 6</a>
+            </li>
+        </ul>
+    </nav>
+</header>
---
+++ php-exercises/exercise_5.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include("./assets/php/_head.php"); ?>
+<body>
+    <?php include("./assets/php/_header.php"); ?>
+    <main>
+        <h2>Exercise 5: PHP - Student Grade Indicator</h2>
+        <p>
+            Determines and displays grade division based on a given grade percentage entered by the user.
+            Displays "Invalid Grade" if user input is negative of non-numeric.
+        </p>
+        <form action="" method="post">
+        <?php
+            function gradeDivision($percentage) {
+                if (!is_numeric($percentage) || $percentage < 0) {
+                    return "Invalid Grade";
+                }
+                else if ( $percentage >= 60 ) {
+                    return "First Division";
+                }
+                else if ( $percentage >= 45 ) {
+                    return "Second Division";
+                }
+                else if ( $percentage >= 33 ) {
+                    return "Third Division";
+                }
+                else {
+                    return "Fail";
+                }
+            }
+        ?>
+            <input name="grade_percentage" id="grade_percentage"
+            <?php 
+            if (isset($_POST['grade_percentage'])) {
+                echo ' value="'.$_POST['grade_percentage'].'"';
+            }
+            ?>
+            >
+            <label for="grade_percentage">Grade percentage</label><br>
+
+            <input name="grade_division" id="grade_division"
+            <?php 
+            if (isset($_POST['grade_division'])) {
+                echo ' value="'.gradeDivision($_POST['grade_percentage']).'"';
+            }
+            ?>
+            >
+            <label for="grade_division">Grade division</label><br>
+
+            <input type="submit" value="Determine grade division">
+        </form>
+    </main>
+</body>
+</html>
