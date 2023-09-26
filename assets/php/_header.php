480ed9aa52bbe02ab853e194653cc9b8e6870a18
Benny Mattis <37753727+wbmattis2@users.noreply.github.com>
2023-09-26 09:12:33 -0400

    Delete assets directory

--- assets/php/_header.php
+++
@@ -1,9 +0,0 @@
-
-        <ul>
-            <li>
-                <a href="./php_exercises">PHP Demonstrations</a>
-            </li>
-			<li>
-                <a href="./expense_tracker">Expense Tracker Application</a>
-            </li>
-        </ul>
